89eca59781de4fed1ca06ab0069aa9501b2e4215
JSpeed8th <jspeed8th@gmail.com>
2018-01-04 18:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ const returnLastTwoDrivers = function (drivers) {
   return [drivers[drivers.length - 2], drivers[drivers.length - 1] ];
 };
 
-const selectingDrivers = [ 
-  const returnFirstTwoDrivers = function(drivers) {
-      return [drivers[0], drivers[1] ];
-    }
-];
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
