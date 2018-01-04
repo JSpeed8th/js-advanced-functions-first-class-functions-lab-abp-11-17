7056b87d99425b2642428c3f03a6658ee03dc31f
JSpeed8th <jspeed8th@gmail.com>
2018-01-04 17:28:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  const firstTwoDrivers = function(drivers) {
+    const firstDriver = drivers[0];
+    const secondDriver = drivers[1];
+    return {firstDriver, secondDriver};
+  }
+}
