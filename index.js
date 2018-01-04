60c3f30aa10a2e152056b052adea4c8d487cfea8
JSpeed8th <jspeed8th@gmail.com>
2018-01-04 19:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function createFareMultiplier(integer) {
   }
 };
 
-const fareDoubler = createFareMultiplier();
+const fareDoubler = createFareMultiplier() - integer;
 
 
 function selectDifferentDrivers (drivers, fn) {
