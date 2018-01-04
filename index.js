15ce83ee51a7b0424f85aaf758aae651f786ffb8
JSpeed8th <jspeed8th@gmail.com>
2018-01-04 17:36:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Code your solution in this file!
-function returnFirstTwoDrivers(drivers) {
-  const firstTwoDrivers = driver.map(function(driver) {
-    const firstDriver = drivers[0];
-    const secondDriver = drivers[1];
-    console.log(firstDriver);
-    console.log(secondDriver);
-    return {firstDriver, secondDriver});
-  }
+const result = function (drivers) {
+  return drivers[0];
 }
